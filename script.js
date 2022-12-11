8f2508c662a435a6fc8ced9125c0f6ea49ec1195
lazyplayboy <119006201+lazyplayboy@users.noreply.github.com>
2022-12-11 19:25:52 +0530

    day 06 task
    day 06 task

---
+++ script.js
@@ -0,0 +1,48 @@
+program 1
+
+
+class  ==> person
+
+class person { 
+
+    constructor (age,salary,name,gender)
+
+    this.age = 24
+    this.salary = 25000
+    this.name = arun
+    this gender = male
+
+   }
+}
+
+var test = new person ( "24","25000",arun,male)
+
+console.log  (test.age);
+console.log  (test.salary);
+console.log  (test.name);
+console.log  (test.gender);
+
+
+
+
+
+
+class
+
+function Person() {
+
+    this.firstName = "arun";
+    this.lastName = "msd";
+}
+
+var person1 = new Person();
+person1.firstName = "arun";
+person1.lastName = "msd";
+            
+alert(person1.firstName + " " + person1.lastName);
+
+var person2 = new Person();
+person2.firstName = "vijay";
+person2.lastName = "star";
+            
+alert(person2.firstName + " " + person2.lastName );
